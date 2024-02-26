5100d3a9a6d6a67427da49411ac596573a3a0cb1
Rafael Uchôa <rafael+github@gmail.com>
2024-02-26 20:38:53 -0300

    Update treasure location and add winners in seeds.rb

--- config/initializers/treasure_config.rb
+++ config/initializers/treasure_config.rb
@@ -1,7 +1,25 @@
+# The Hotel Globo, located at -7.112778, -34.888889 in João Pessoa, Paraíba, Brazil, boasts a rich history:
+
+# Past:
+
+# Constructed in 1929, it served as a prominent hotel for decades.
+# Known for its elegant architecture and central location, it was a popular spot for both local high society and visitors.
+# Witnessed significant historical events during its time as a hotel.
+# Present:
+
+# No longer operates as a hotel, having transitioned to a cultural center.
+# Offers various cultural activities, including art exhibitions, book launches, and presentations.
+# Maintains its historical significance and architectural charm, serving as a reminder of João Pessoa's past.
+# In essence:
+
+# Hotel Globo transformed from a thriving hotel to a cultural hub, yet it continues to be a significant landmark in João Pessoa's history and cultural scene.
+
+# It's close to the Praça Dom Ulrico, where the city was founded in 1585.
+
 Rails.configuration.treasure = {
   coordinates: {
-    latitude: ENV['TREASURE_LATITUDE']&.to_f || 40.416775,
-    longitude: ENV['TREASURE_LONGITUDE']&.to_f || -3.703790,
+    latitude: ENV['TREASURE_LATITUDE']&.to_f || -7.112778,
+    longitude: ENV['TREASURE_LONGITUDE']&.to_f || -34.888889,
   },
   radius: ENV['TREASURE_RADIUS']&.to_i || 1000, # meters
 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Winner.create!(user: FactoryBot.create(:user), latitude: -7.112778, longitude: -34.888889) # Treasure Location!
+Winner.create!(user: FactoryBot.create(:user), latitude: -7.117782, longitude: -34.888389)
+Winner.create!(user: FactoryBot.create(:user), latitude: -7.118782, longitude: -34.888389)
+Winner.create!(user: FactoryBot.create(:user), latitude: -7.118782, longitude: -34.888389)
+Winner.create!(user: FactoryBot.create(:user), latitude: -7.119782, longitude: -34.888389)
+
+puts "Done!"
+puts "Run the following command to see the winners:"
+puts "curl -X GET \"http://localhost:3000/winners?per_page=2\" -H \"accept: application/json\""
+# curl -X GET "http://localhost:3000/winners?per_page=2" -H "accept: application/json"
